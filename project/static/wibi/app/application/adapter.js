b649de4758d519a16aa69da63297bbdf9ca1aecb
Nate Nichols <nathann@ori.org>
2015-04-07 17:39:03 -0700

    Ember updates!

--- project/static/wibi/app/application/adapter.js
+++ project/static/wibi/app/application/adapter.js
@@ -4,3 +4,20 @@ export default DS.RESTAdapter.extend({
   coalesceFindRequests: true,
   namespace: 'api'
 });
+
+//test!!!
+// I see you! :) :)
+
+// http://virtualenvwrapper.readthedocs.org/en/latest/index.html#introduction
+
+
+// sudo pip install virtualenvwrapper
+
+// export WORKON_HOME=~/.virtualenvs
+// source /usr/local/bin/virtualenvwrapper.sh
+
+// mkvirtualenv wibi
+// pip freeze
+// workon wibi
+// pip freeze
+// pip install -r requirements.txt
