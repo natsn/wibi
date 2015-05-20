69e47936b15f3379df635dabbb651922c92be36b
Borgaard <nicoleborgaard@gmail.com>
2015-05-20 11:07:07 -0700

    yay infinite loop squished and Angular app works as expected

--- project/static/wibi/app/router.js
+++ project/static/wibi/app/router.js
@@ -1,6 +1,6 @@
 angular.module('wibi-routes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/', {
-      templateUrl: '/templates/application.html',
+      templateUrl: '/static/templates/application.html',
       controller: 'homeCtrl'
     })
     $locationProvider.html5Mode(true); //enables url change abilities without refreshing browser
